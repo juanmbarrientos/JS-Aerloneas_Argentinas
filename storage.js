161729d1dd24ab6a44b52fab81789282013997d0
Juan Barrientos <109054384+juanmbarrientos@users.noreply.github.com>
2023-03-11 00:52:01 -0300

    Actualización de Lógica

--- storage.js
+++ storage.js
@@ -1,140 +1,170 @@
-function guardar(){
-    
-    // captura los inputs
-    let txtNombreInput = document.getElementById("txtNombre");
-    let txtApellidoInput = document.getElementById("txtApellido");
-    let txtEmailInput = document.getElementById("txtEmail");
-    let txtPassInput = document.getElementById("txtPass");
-    
-    // captura los valores de los inputs
+function validar() {
+  let txtNombreInput = document.getElementById("txtNombre");
+  let txtApellidoInput = document.getElementById("txtApellido");
+  let txtEmailInput = document.getElementById("txtEmail");
+  let txtPassInput = document.getElementById("txtPass");
+  let ayudaNombre = document.getElementById("ayudaNombre")
+  let ayudaApellido = document.getElementById("ayudaApellido")
+  let ayudaMailVacio = document.getElementById("ayudaMailVacio")
+  let ayudaMailIncorrecto = document.getElementById("ayudaMailIncorrecto")
+  let txtNombre = document.getElementById("txtNombre").value;
+  let txtApellido = document.getElementById("txtApellido").value;
+  let txtEmail = document.getElementById("txtEmail").value;
+  let txtPass = document.getElementById("txtPass").value;
+
+  // Validar campo Nombre
+  if (txtNombre.trim() == "") {
+    // alert("El campo Nombre es requerido");
+    txtNombreInput.classList.add("campo_incorrecto");
+    ayudaNombre.classList.remove("ayudaNombre");
+    // return false;
+  } else {
+    txtNombreInput.classList.add("campo_correcto");
+    ayudaNombre.classList.add("ayudaNombre");
+  }
+
+  // Validar campo Apellido
+  if (txtApellido.trim() == "") {
+    // alert("El campo Apellido es requerido");
+    txtApellidoInput.classList.add("campo_incorrecto");
+    ayudaApellido.classList.remove("ayudaApellido");
+    // return false;
+  } else {
+    txtApellidoInput.classList.add("campo_correcto");
+    ayudaApellido.classList.add("ayudaApellido")
+
+  }
+
+  // Validar campo Email
+  if (txtEmail.trim() == "") {
+    // alert("El campo Email es requerido");
+    txtEmailInput.classList.add("campo_incorrecto");
+    ayudaMailVacio.classList.remove("ayudaMailVacio");
+    ayudaMailIncorrecto.classList.add("ayudaMailIncorrecto");
+    // return false;
+  } else if (!/\S+@\S+\.\S+/.test(txtEmail)) {
+    // alert("Ingrese un correo electrónico válido");
+    ayudaMailVacio.classList.add("ayudaMailVacio");
+    txtEmailInput.classList.add("campo_incorrecto");
+    ayudaMailIncorrecto.classList.remove("ayudaMailIncorrecto");
+    // return false;
+  } else {
+    txtEmailInput.classList.add("campo_correcto");
+    ayudaMailVacio.classList.add("ocultarAdvertencia");
+    ayudaMailIncorrecto.classList.add("ocultarAdvertencia");
+    // ayudaMailVacio.classList.remove("ayudaEmailVacio");
+    // ayudaMailIncorrecto.classList.remove("ayudaEmailIncorrecto");
+  }
+
+  // Validar campo Contraseña
+  if (txtPass.trim() == "") {
+    // alert("El campo Contraseña es requerido");
+    txtPassInput.classList.add("campo_incorrecto");
+    return false;
+  } else if (txtPass.length > 10) {
+    // alert("La contraseña debe tener máximo 10 caracteres");
+    txtPassInput.classList.add("campo_incorrecto");
+    return false;
+  } else if (
+    !/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(txtPass)
+  ) {
+    // alert(
+    //   "La contraseña debe incluir al menos un número, un símbolo y una letra mayúscula y minúscula"
+    // );
+    txtPassInput.classList.add("campo_incorrecto");
+    return false;
+  } else {
+    txtPassInput.classList.add("campo_correcto");
+  }
+
+  // Si todos los campos son válidos, retornar true
+  return true;
+}
+
+function guardar() {
+  if (validar()) {
     let txtNombre = document.getElementById("txtNombre").value;
     let txtApellido = document.getElementById("txtApellido").value;
     let txtEmail = document.getElementById("txtEmail").value;
     let txtPass = document.getElementById("txtPass").value;
 
-    if (txtNombre !== "" || txtApellido !== "" || txtEmail !== "" || txtPass !== ""){
-        alert("Los datos se guardaron exito")
-        sessionStorage.setItem("nombre",JSON.stringify(txtNombre));
-        sessionStorage.setItem("apellido",JSON.stringify(txtApellido));
-        sessionStorage.setItem("email",JSON.stringify(txtEmail));
-        sessionStorage.setItem("password",JSON.stringify(txtPass));
-        txtNombreInput.classList.add("campo_correcto");
-        txtApellidoInput.classList.add("campo_correcto");
-        txtEmailInput.classList.add("campo_correcto");
-        txtPassInput.classList.add("campo_correcto");
-    }
+    alert("Los datos se guardaron exitosamente");
+    sessionStorage.setItem("nombre", JSON.stringify(txtNombre));
+    sessionStorage.setItem("apellido", JSON.stringify(txtApellido));
+    sessionStorage.setItem("email", JSON.stringify(txtEmail));
+    sessionStorage.setItem("password", JSON.stringify(txtPass));
+  }
 }
 
-        // if (txtNombre === "" || txtApellido === "" || txtEmail === "" || txtPass === ""){
-        //     alert("Todos los campos deben estar completos");
-        //     if(txtNombre === ""){
-        //         alert("Debe completar el campo Nombre");
-        //         txtNombreInput.classList.add("bg-danger");
-        //     }else if (txtApellido === ""){
-        //         alert("Debe completar el campo Apellido");
-        //         txtApellidoInput.classList.add("bg-danger");
-        //     }else if (txtEmail === ""){
-        //         alert("Debe completar el campo eMail");
-        //         txtEmail2.classList.add("bg-danger");
-        //     }else if (txtPass === ""){
-        //         alert("Debe completar el campo Contraseña");
-        //         txtPassInput.classList.add("bg-danger");
-        //     }else{
-        //     sessionStorage.setItem("nombre",JSON.stringify(txtNombre));
-        //     sessionStorage.setItem("apellido",JSON.stringify(txtApellido));
-        //     sessionStorage.setItem("email",JSON.stringify(txtEmail));
-        //     sessionStorage.setItem("password",JSON.stringify(txtPass));
-        //     alert("Los datos se guardaron exito")
-        // }
-    
-    
-        
-       
-    
+function verificar() {
+  // captura los valores de los inputs
+  let loginEmail = document.getElementById("loginEmail").value;
+  let loginPassword = document.getElementById("loginPassword").value;
 
-    
+  // captura los inputs
+  let loginEmailInput = document.getElementById("loginEmail");
+  let loginPasswordInput = document.getElementById("loginPassword");
 
-    // funcion inutilizable, solo mostraba el nombre para verificar
-    // function resultados(){
-    //     let resultados = document.getElementById("txtResultados");
-    //     let resNombre =  JSON.parse(sessionStorage.getItem("nombre"));
-    //     resultados.innerHTML = resNombre;
-    // }
+  // compara con el SessionStorage
+  let storageNombre = JSON.parse(sessionStorage.getItem("nombre"));
+  let storageApellido = JSON.parse(sessionStorage.getItem("apellido"));
+  let storageEmail = JSON.parse(sessionStorage.getItem("email"));
+  let storagePassword = JSON.parse(sessionStorage.getItem("password"));
 
-    function verificar(){
-        // captura los valores de los inputs
-        let loginEmail = document.getElementById("loginEmail").value;
-        let loginPassword = document.getElementById("loginPassword").value;
-
-        // captura los inputs
-        let loginEmailInput = document.getElementById("loginEmail");
-        let loginPasswordInput = document.getElementById("loginPassword");
-        
-        // compara con el SessionStorage
-        let storageNombre =  JSON.parse(sessionStorage.getItem("nombre"));
-        let storageApellido =  JSON.parse(sessionStorage.getItem("apellido"));
-        let storageEmail =  JSON.parse(sessionStorage.getItem("email"));
-        let storagePassword =  JSON.parse(sessionStorage.getItem("password"));
-    
-        if (loginEmail === storageEmail && loginPassword === storagePassword){
-            loginEmailInput.classList.add("campo_correcto");
-            loginPasswordInput.classList.add("campo_correcto")
-            alert(`Bienvenido/a ${storageNombre} ${storageApellido}`),
-            window.location.href = "main.html";
-        }else{
-            alert("ACCESO DENEGADO, VUELVA A INTENTAR");
-            if(loginEmail !== storageEmail && loginPassword !== storagePassword){
-                loginEmailInput.classList.add("campo_vacio");
-                loginPasswordInput.classList.add("campo_vacio");
-            }else if(loginEmail !== storageEmail){
-                    loginEmailInput.classList.add("campo_vacio");
-                    loginPasswordInput.classList.add("campo_correcto");
-                }else{
-                    loginPasswordInput.classList.add("campo_vacio");
-                    loginEmailInput.classList.add("campo_correcto");
-                }
-        }
+  if (loginEmail === storageEmail && loginPassword === storagePassword) {
+    loginEmailInput.classList.add("campo_correcto");
+    loginPasswordInput.classList.add("campo_correcto");
+    alert(`Bienvenido/a ${storageNombre} ${storageApellido}`),
+      (window.location.href = "main.html");
+  } else {
+    alert("ACCESO DENEGADO, VUELVA A INTENTAR");
+    if (loginEmail !== storageEmail && loginPassword !== storagePassword) {
+      loginEmailInput.classList.add("campo_vacio");
+      loginPasswordInput.classList.add("campo_vacio");
+    } else if (loginEmail !== storageEmail) {
+      loginEmailInput.classList.add("campo_vacio");
+      loginPasswordInput.classList.add("campo_correcto");
+    } else {
+      loginPasswordInput.classList.add("campo_vacio");
+      loginEmailInput.classList.add("campo_correcto");
     }
+  }
+}
 
-    function logout(){
-        alert("gracias por visitarnos");
-        // window.localStorage.clear();
-        // window.location.reload(true);
-        window.location.replace('index.html');
-    }
+function logout() {
+  alert("gracias por visitarnos");
+  // window.localStorage.clear();
+  // window.location.reload(true);
+  window.location.replace("index.html");
+}
 
-
-    
-    
- 
 // traerDatos() se ejecuta solo - traerDatos solo se ejecuta cuando le haces clic
 document.onload = traerDatos();
 // document.querySelector("#boton").addEventListener('click', traerDatos());
 
-function traerDatos(){
-    // console.log("dentro de la funcion");
+function traerDatos() {
+  // console.log("dentro de la funcion");
 
-    const xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', 'vuelos.json', true);
-    xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let datos = JSON.parse(this.responseText)
-            // console.log(datos);
+  xhttp.open("GET", "vuelos.json", true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let datos = JSON.parse(this.responseText);
+      // console.log(datos);
 
-            let res = document.querySelector('#row_vuelos');
-            res.innerHTML= '';
+      let res = document.querySelector("#row_vuelos");
+      res.innerHTML = "";
 
-            const txtNombreUser = document.getElementById("txtNombreUser");
-            const txtNombreUser2 = document.getElementById("txtNombreUser2");
-    
-    
-            txtNombreUser2.textContent = (JSON.parse(sessionStorage.getItem("nombre")));
-            txtNombreUser.textContent = (JSON.parse(sessionStorage.getItem("nombre")));
+      const txtNombreUser = document.getElementById("txtNombreUser");
+      const txtNombreUser2 = document.getElementById("txtNombreUser2");
 
-            for(let item of datos){
-                res.innerHTML += `
+      txtNombreUser2.textContent = JSON.parse(sessionStorage.getItem("nombre"));
+      txtNombreUser.textContent = JSON.parse(sessionStorage.getItem("nombre"));
+
+      for (let item of datos) {
+        res.innerHTML += `
                 <div class="col col-lg-3 col-md-6 col-12">               
                     <div class="card rounded mb-5">
                         <img src="${item.foto}" class="vuelo_card" alt="...">
@@ -160,53 +190,42 @@ function traerDatos(){
                             </div>
                             <div class="precio_comprar">
                                 <p id="precio" class="precio">$${item.precio}</p>
-                                <button type="submit"class="btn btn-warning" onclick="comprar()">¡Comprar! </button>
-                            </div>
+                                <button type="button" class="btn btn-warning" onclick="comprar(this)">¡Comprar! </button>
+                                </div>
                         </div>
                     </div>
                 </div>
-                `
-            }
-        }    
-    }    
-}    
-
-let array_vuelo = []
-
-function comprar(){
-
-        const id = document.getElementById("promo").textContent
-        const empresa = document.getElementById("empresa").textContent;
-        const ruta = document.getElementById("ruta").textContent;
-        let ida = document.getElementById("ida").value;
-        let vuelta = document.getElementById("vuelta").value;
-        const precio = document.getElementById("precio").textContent;
-
-        const arr_vue = {
-            "id": id,
-            "buss": empresa,
-            "rut": ruta,
-            "idaa": ida,
-            "vuelt": vuelta,
-            "prec": precio, 
-        }
-
-        // ref 1
-        array_vuelo.push(arr_vue);
-        localStorage.setItem(id, JSON.stringify(array_vuelo));
-        
-        // localStorage.setItem("empresa", JSON.stringify(empresa));
-        // localStorage.setItem("ruta", JSON.stringify(ruta));
-        // localStorage.setItem("ida", JSON.stringify(ida));
-        // localStorage.setItem("vuelta", JSON.stringify(vuelta));
-        // localStorage.setItem("precio", JSON.stringify(precio));
-
-        alert("se agregó correctamente");
+                `;
+      }
     }
+  };
+}
 
-// ref 1
+// let array_vuelo = [];
 
+function comprar(btn) {
+  let card = btn.closest(".card");
+  let id = card.querySelector(".promo").textContent;
+  let empresa = card.querySelector(".empresa").textContent;
+  let ruta = card.querySelector(".ruta").textContent;
+  let ida = card.querySelector("#ida").value;
+  let vuelta = card.querySelector("#vuelta").value;
+  let precio = card.querySelector(".precio").textContent;
 
+  const arr_vue = {
+    id: id,
+    empresa: empresa,
+    ruta: ruta,
+    ida: ida,
+    vuelta: vuelta,
+    precio: precio,
+  };
 
+  //Agregado
+  let array_vuelo = JSON.parse(localStorage.getItem(id)) || [];
 
+  array_vuelo.push(arr_vue);
+  localStorage.setItem(id, JSON.stringify(array_vuelo));
 
+  alert("El vuelo se agregó correctamente");
+}
